75083c587eb6a4d0ddf512f307f8fefdb5f22951
fatmaazra.yildiz <fatmaazra.yildiz@DE-M087797.local>
2022-11-24 14:11:23 +0100

    Added quantity, remove and clear cart methods

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,7 +6,7 @@ import { Cart, CartItem } from '../models/cart.model';
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
+export class CartService {  
   cart = new BehaviorSubject<Cart>({items: []});
   constructor(private _snackBar: MatSnackBar) { }
 
@@ -22,13 +22,40 @@ export class CartService {
   this.cart.next({ items });
   this._snackBar.open('1 item added to cart.', 'ok', { duration: 3000 }); 
 }
+removeQuantity(item: CartItem): void{
+  let itemForRemoval: CartItem | undefined;
+  let filteredItems = this.cart.value.items.map((_item) =>{
+    if (_item.id === item.id){
+      _item.quantity --;
+      if(_item.quantity === 0){
+        itemForRemoval = _item; 
+      }
+    }
+    return _item;
+  }) 
+  if (itemForRemoval){
+   filteredItems = this.removeFromCart(itemForRemoval, false); 
+  }
+  this._snackBar.open('1 item removed from cart')
+}
 getTotal(items: Array<CartItem>): number {
-  return items.
-  map((item) => item.price * item.quantity)
+  return items
+  .map((item) => item.price * item.quantity)
   .reduce((prev, current) => prev + current, 0); 
 }
 clearCart(): void {
   this.cart.next({items: []});
-  this._snackBar.open('1 item removed from cart', 'ok', { duration: 3000 });
+  this._snackBar.open('1 item removed from cart', 'ok', { duration: 3000, });
 }
+
+removeFromCart(item: CartItem, update = true): Array<CartItem> {
+  const filteredItems = this.cart.value.items.filter(
+    (_item) => _item.id !== item.id
+  );
+  if (update){
+    this.cart.next({items: filteredItems});
+    this._snackBar.open('1 item removed from cart', 'ok', {duration: 3000}); 
+  }
+    return filteredItems; 
+  }
 }
